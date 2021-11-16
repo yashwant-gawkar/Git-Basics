2d5185ed530801000339051b946befc759bb5483
Yashwant Gawkar <yashwant.gawkar@gmail.com>
2021-11-16 23:53:49 +0530

    Updating Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String args[]){
         int a=7;
         int b=8;
-        System.out.println(a+b);
+        System.out.println("The sum is: "+a+b);
     }
 }
