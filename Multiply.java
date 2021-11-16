cb81ec0b76a950d8d6ee1e1f89eaa60a3b391134
Yashwant Gawkar <yashwant.gawkar@gmail.com>
2021-11-17 00:11:48 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a=4,b=2;
+        System.out.println("The product is: "+a*b);
+    }
+    
+}
